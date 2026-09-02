ed2fe8f67e994757ca2775a5363dcf52d224c654
Lennart Martens <lmartens@metanous.be>
2026-09-02 23:08:02 +0200

    PWA

--- zaal/src/main.ts
+++ zaal/src/main.ts
@@ -4,3 +4,8 @@ import { App } from './app/app';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
+
+// Minimale PWA: enkel om de app installeerbaar te maken, zonder caching.
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register(new URL('sw.js', document.baseURI));
+}
